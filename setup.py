010b69cacf6d24c4068831295b63e8522c86b967
David de Koning <David.deKoning@arup.com>
2017-02-05 21:21:18 -0500

    Added setup.py and rejiggered the directory to allow installation via pip.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='cashflow',
+      version='1.0',
+      description='Python library to generate future cashflows for budgeting.',
+      url='https://github.com/daviddekoning/cashflow',
+      author='David de Koning',
+      license='MIT',
+      packages=['cashflow'],
+      zip_safe=False)
